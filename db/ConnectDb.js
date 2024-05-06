2fae54186f3b348c4ac53b1b9a5f2471495c3803
mannumishra <mannumishra2207@outlook.com>
2024-05-06 16:02:57 +0530

    add change

--- db/ConnectDb.js
+++ db/ConnectDb.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose")
 
 const connectDb = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/presties")
-        // await mongoose.connect(process.env.MONGO_URL)
+        // await mongoose.connect("mongodb://localhost:27017/presties")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("Database connectd Successfully");
     } catch (error) {
         console.log(error);
